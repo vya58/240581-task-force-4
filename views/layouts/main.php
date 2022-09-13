b164c96c3e9af467005f8909e4b1ed559b047e7b
vya58 <vya58@yandex.ru>
2022-09-13 23:11:16 +0300

    Первый шаг на большом пути

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -75,55 +75,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => '@w
 </header>
 <main class="container container--registration">
         <?php $content ?>
-    <div class="center-block">
-        <div class="registration-form regular-form">
-            <form>
-                <h3 class="head-main head-task">Регистрация нового пользователя</h3>
-                <div class="form-group">
-                    <label class="control-label" for="username">Ваше имя</label>
-                    <input id="username" type="text">
-                    <span class="help-block">Error description is here</span>
-                </div>
-                <div class="half-wrapper">
-                    <div class="form-group">
-                        <label class="control-label" for="email-user">Email</label>
-                        <input id="email-user" type="email">
-                        <span class="help-block">Error description is here</span>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label" for="town-user">Город</label>
-                        <select id="town-user">
-                            <option>Москва</option>
-                            <option>Санкт-Петербург</option>
-                            <option>Владивосток</option>
-                        </select>
-                        <span class="help-block">Error description is here</span>
-                    </div>
-                </div>
-                <div class="half-wrapper">
-                <div class="form-group">
-                    <label class="control-label" for="password-user">Пароль</label>
-                    <input id="password-user" type="password">
-                    <span class="help-block">Error description is here</span>
-                </div>
-                </div>
-                <div class="half-wrapper">
-
-                <div class="form-group">
-                    <label class="control-label" for="password-repeat-user">Повтор пароля</label>
-                    <input id="password-repeat-user" type="password">
-                    <span class="help-block">Error description is here</span>
-                </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label checkbox-label" for="response-user">
-                        <input id="response-user" type="checkbox" checked>
-                        я собираюсь откликаться на заказы</label>
-                </div>
-                <input type="submit" class="button button--blue" value="Создать аккаунт">
-            </form>
-        </div>
-    </div>
+    
 </main>
 </body>
 </html>
