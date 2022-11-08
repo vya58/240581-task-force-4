0fe087eb5b6c6d118f911933ce409a2e58d10fac
vya58 <vya58@yandex.ru>
2022-11-08 20:58:27 +0300

    Убрал лишнее в Site

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,12 +2,9 @@
 
 namespace app\controllers;
 
-use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\forms\ContactForm;
 
 class SiteController extends Controller
 {
@@ -51,44 +48,5 @@ class SiteController extends Controller
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
-    }
-
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    /*
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
-*/
-    /**
-     * Displays contact page.
-     *
-     * @return Response|string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
     }
 }
--- views/site/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/** @var yii\web\View $this */
-
-$this->title = 'My Yii Application';
-?>
